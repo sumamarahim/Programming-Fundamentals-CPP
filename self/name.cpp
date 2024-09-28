e3bd4d348fb892260f44ff2bf01dd4cbea9dab51
Sumama Rahim <sumama.rahim@yandex.com>
2024-09-28 07:30:59 +0500

    Update name.cpp

--- self/name.cpp
+++ self/name.cpp
@@ -1,15 +1,15 @@
 #include <iostream>
 
-// Just hello!
-// 1. Write a c++ program that print your name on terminal
 
 int main() {
-	
+	// 1. Write a C++ program that prints your name on the terminal
+
+	// program 1 starts from here
 	// to print name on terminal
-	std::cout<<"my name is Sumama R.";
+	std::cout<<"My name is Sumama R.";
+	// program 1 ends here
 
-
-	// compare two numbers
+	//2. Write a C++ program that compares two numbers
 	int num1 = 3;
 	int num2 = 6;
 	std::cout<<"\nThe 1st number is: "<<num1<<"\nThe 2nd number is: "<<num2<<std::endl;
