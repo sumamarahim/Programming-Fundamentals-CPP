70114bc942ee3aefaf72bddabad43c9687ac7117
Sumama Rahim <sumama.rahim@yandex.com>
2024-11-09 16:57:07 +0500

    Create practical 10 and 11.cpp

---
+++ Lab/lab-mid-term-exam/practical 10 and 11.cpp
@@ -0,0 +1,153 @@
+/***
+	Name: Sumama Rahim
+	Reg# F24BSCS-039
+	Programming Fundamentals - Lab Manual
+	BS CS F24 Morning Section A
+	--------------------------------------
+	
+	Practical No. 10, 11: Repetitive/ Iterative Logic, Loops
+
+*/
+
+// Start of Program
+
+#include <iostream>
+using namespace std;
+
+int main(){
+//	/**************************** START OF LAB EXERCISE 1 ********************************/
+//	/*
+//		Lab Exercise # 1:
+//			Write a C++ program to Print Table of any Number.
+//
+//	*/
+//	
+//	// Solution
+//	int table_of;
+//	
+//	cout<<"Enter a number to print table for: ";
+//	cin>>table_of;
+//	
+//	for (int i = 1; i <=10; i++)
+//	{
+//		cout<<table_of<<" x "<<i<<"\t = "<<table_of*i<<endl;
+//	}
+//
+	/**************************** END OF LAB EXERCISE 1 ********************************/
+
+	
+//	/**************************** START OF LAB EXERCISE 2 ********************************/
+//	/*		
+//		Lab Exercise # 2:
+//			Write a method with a for loop that computes the sum of first n positive integers: 
+//			sum = 1 + 2 + 3 + … + n
+//	
+//	*/
+//	
+//	// Solution
+//	int n_num, sum = 0;
+//	
+//	cout<<"Enter n number to add all: ";
+//	cin>>n_num;
+//	
+//	for (int i = 1; i<=n_num; i++)
+//	{
+//		sum = sum + i;
+//	}
+//	
+//	cout<<endl<<sum;
+//	
+//
+	/**************************** END OF LAB EXERCISE 2 ********************************/
+
+
+
+
+
+
+
+	
+//	/**************************** START OF LAB EXERCISE 5 ********************************/
+//	/*		
+//		Lab Exercise # 5:
+//			Write a C++ program that will print the following pattern using for Loop.
+//			*
+//			**
+//			***
+//			****
+//			******
+//			*******
+//	*/
+//	
+//	// Solution
+//	
+//	for (int i = 1; i <=7; i++)
+//	{
+//		for (int j = 1; j<=i; j++)
+//		{
+//			cout<<"*";
+//		}
+//	cout<<"\n";
+//	}
+//	
+//	
+	/**************************** END OF LAB EXERCISE 5 ********************************/
+
+
+
+
+
+/**************************** START OF LAB EXERCISE 8 ********************************/
+	/*		
+		Lab Exercise # 8:
+			Write a C++ program that will print the following pattern using for Loop.
+			1 2 3 4
+			1 2 3
+			1 2
+			1
+	*/
+	
+	// Solution
+	
+//	for (int i = 1; i <=7; i++)
+//	{
+//		for (int j = i; j >= 1; j--)
+//		{
+////			cout<<i<<endl;
+//			cout<<j<<endl;
+//		}
+////	cout<<"\n";
+//	}
+//	
+//	
+	
+//	for (int i = 1; i <=7; i++)
+//	{
+//		for (int j = i; j >= 1; j--)
+//		{
+////			cout<<i<<endl;
+//			cout<<j;
+//		}
+//		
+//	cout<<"\n";
+//	}
+	
+
+//
+//	for (int i = 4; i >= 1; i--)
+//	{
+//		for (int j = 1; j <= i; j++)
+//		{
+//			cout<<j;
+//		}
+//		
+//	cout<<"\n";
+//	}
+	
+	
+	/**************************** END OF LAB EXERCISE 8 ********************************/
+
+
+	
+	return 0;
+}
