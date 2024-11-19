f1f5b2dc6bd90b7178a09f90ef43344fe596d574
Sumama Rahim <sumama.rahim@yandex.com>
2024-11-19 09:08:58 -0800

    Create loops1.cpp
    Loops in detail
    1. Do While
    2. While
    3. For
    4. Nested Loops
     and mixture of all with each other
    single program with multiple strategies

---
+++ self/loops in details/loops1.cpp
@@ -0,0 +1,174 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+//	for (int i = 1; i <= 5; i++)
+//	{
+//		for (int j = 5; j >= i; j--)
+//		{
+//			cout<<"*";
+//		}
+//		cout<<endl;
+//	}
+	
+//		//same using while loop
+//	cout<<"\n*** WHILE LOOP ***\n"<<endl;
+	
+//	//	while loop and for loop as inner loop
+//	int i = 1;
+//	while (i <= 5)
+//	{
+//		for (int j = 5; j >= i; j--)
+//		{
+//			cout<<"*";
+//		}
+//		cout<<endl;
+//		i++;
+//	}
+	
+	
+//	//	while loop and while loop as inner loop
+//	int i2 = 1;
+//	while (i2 <= 5)
+//	{
+//		int j = 5;
+//		while (j >= i2)
+//		{
+//			cout<<"*";
+//				j--;
+//		}
+//		cout<<endl;
+//		i2++;
+//	}
+	
+	
+	
+//	//	for loop and while loop as inner loop
+//	
+//		for (int i = 1; i <= 5; i++)
+//		{
+//			int j = 5;
+//		while (j >= i)
+//		{
+//			cout<<"*";
+//				j--;
+//		}
+//		cout<<endl;
+//		}
+	
+	
+		//	same using do while loop
+	cout<<"\n*** DO WHILE LOOP ***\n"<<endl;
+	
+//	//	do while loop and for loop as inner loop
+//	
+//	int i = 1;
+//	do
+//	{
+//		for (int j = 5; j >= i; j--)
+//		{
+//			cout<<"*";
+//		}
+//		cout<<endl;
+//		
+//		i++;
+//	}
+//	while(i <= 5);
+	
+//	//	do while loop and while as inner loop
+//	int i = 1;
+//	do 	
+//	{
+//		int j = 5;
+//		while (j >= i)
+//		{
+//			cout<<"*";
+//			j--;
+//		}
+//		cout<<endl;
+//		i++;
+//	}
+//	while(i <= 5);
+//	
+	
+//	//	while loop and do while loop as inner loop
+//	
+//	int i = 1; 
+//	while (i <= 5)
+//	{
+//		int j = 5;
+//		do
+//		{
+//			cout<<"*";
+//			j--;
+//		}
+//		while(j >= i);
+//		cout<<endl;
+//		i++;
+//	}
+	
+	
+	
+//	//	for loop and do while loop as inner loop
+//	
+//	for (int i = 1; i <= 5; i++)
+//	{
+//		int j = 5;
+//		do
+//		{
+//			cout<<"*";
+//			j--;
+//		}
+//		while(j >= i);
+//		cout<<endl;
+//	}
+	
+//	//	do while loop and do while loop as inner loop
+//	int i = 1;
+//	
+//	do
+//	{
+//		int j = 5;
+//		do 
+//		{
+//			cout<<"*";
+//			j--;
+//		}
+//		while (j >= i);
+//		cout<<endl;
+//		i++;
+//	}
+//	while (i <= 5);
+	
+	
+//	//	while loop and while loop as inner loop
+//	int i2 = 1;
+//	while (i2 <= 5)
+//	{
+//		int j = 5;
+//		while (j >= i2)
+//		{
+//			cout<<"*";
+//				j--;
+//		}
+//		cout<<endl;
+//		i2++;
+//	}
+//	
+//	
+//	
+//	//	for loop and while loop as inner loop
+//	
+//		for (int i = 1; i <= 5; i++)
+//		{
+//			int j = 5;
+//		while (j >= i)
+//		{
+//			cout<<"*";
+//				j--;
+//		}
+//		cout<<endl;
+//		}
+	
+	return 0;
+}
