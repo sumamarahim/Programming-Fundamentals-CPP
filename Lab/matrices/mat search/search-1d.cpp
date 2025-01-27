48e2e7151ed6d78ecdb974005276487deb0dde7a
Sumama Rahim <informtosufyan@gmail.com>
2025-01-27 11:28:59 -0500

    Create search-1d.cpp

---
+++ Lab/matrices/mat search/search-1d.cpp
@@ -0,0 +1,22 @@
+#include <iostream> // input and output stream
+#include <fstream> // file stream
+using namespace std;
+
+int main()
+{
+/*
+  // searching element in 1-D array;
+  int a[5] = {1, 2, 3, 7, 5};
+  int search_num; // to store element, you want to search
+  cout<<"What num do you want to search?"<<endl;
+  cin>>search_num;
+  for (int i = 0; i < 5; i++)
+  {
+    if (search_num == a[i])
+    {
+      cout<<search_num<<" found in array"<<endl;
+    }
+  }
+  
+  return 0;
+}
